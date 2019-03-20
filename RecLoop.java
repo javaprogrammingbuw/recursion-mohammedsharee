46154e160c4be7d4a8b7be88628f59486fef7288
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-20 17:31:55 +0100

    Update RecLoop.java

--- RecLoop.java
+++ RecLoop.java
@@ -2,37 +2,51 @@ import java.util.Scanner;
 
 public class RecLoop{
 
-	//todo: Look at the given code and try to understand the methods.
-	//The first method euklidRek uses recursion to compute the euclidean algorithm.
-	//You can look up the algorithm here: https://en.wikipedia.org/wiki/Euclidean_algorithm
-	//Add another method euclidIterative that computes the euclidean algorithm iterative.
-	//The second method powIterative computes the the base to the power of n in a iterative way.
-	//Add another method powRek which does exactly the same, but by using recursion.
+	
+	
+	 public static void main(String[] args) {
+	    	
+	    	Scanner scan =new Scanner(System.in);
+	    	
+//	    	System.out.println("Enter the value of A");
+	    	System.out.println("Enter the value of base"); 
+//	    	int a = scan.nextInt();
+	    	int base = scan.nextInt();
+//	    	System.out.println("Enter the value of b");
+	    	System.out.println("Enter the value of power");
+//	    	int b = scan.nextInt();
+	    	int pow = scan.nextInt();
+	    	
+//	    	int euclid = euclidIterative(a,b);
+	    	double euc = powRecursive(base,pow);
+	    	System.out.println(euc);
+	 }
+	    	
+	
+	 public static int euclidIterative(int a, int b) {
 
-	 public static int euclidRec(int a, int b) {
-		if (a == b) {
-			return a;
-		} else if (b > a) {
-			int temp = a;
-			a = b;
-			b = temp;
-			return euclidRec(a, b);
-		} else {
-			return euclidRec(a - b, b);
-		}
-	}
 
-    public static double powIterative(final double base, final int n) {
-		if (n < 0) {
-			System.out.println("(Our version of) power is undefined for negative numbers.");
-			return -1;
-		} else {
-			double result = 1; 
+	        while (a != b) {
+	        	if(a > b)
+	                a = a - b;
+	            else
+	                b = b - a;
+	        }
 
-			for (int i = n; i > 0; --i) {
-				result = result * base;
-			}
-			return result;
-		}
-	}
-}
+	        System.out.printf("GCD of given numbers is" + a);
+			return b;
+	    }
+
+	 public static double powRecursive(final double base, final int pow) {
+		 if (pow < 0) {
+				System.out.println("Values are negative");
+				return -1;
+			  }else if (pow==0) {
+				  return 1;
+				  
+			  }else {
+				  return base*powRecursive(base,(pow-1));
+			  }
+	   }
+	 }		 
+		
