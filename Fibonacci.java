f2da0b87edf0afc2f2e2229d6a5fc18147ac0fe5
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-20 17:31:12 +0100

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,54 @@
 
+import java.util.Scanner;
 public class Fibonacci {
-	//todo: The Fibonacci sequence is a sequence where the current number is the sum of the previous numbers. You can read it up here:
-	//https://en.wikipedia.org/wiki/Fibonacci_number
-	//Write an iterative method fib which takes an integer as parameter. This integer corresponds to the position in the Fibonacci sequence.
-	//fib should return the fibonacci number at this position. For example, fib(4) should return 3.
-	//Now, write a second methond fibRek. fibRek does exactly the same like fib, but it works recursive.
-}
+	
+	
+    public static void main(String[] args) {
+    	
+    	Scanner scan =new Scanner(System.in);
+    	
+    	System.out.println("Enter the Number");
+    	int n = scan.nextInt();
+    	int fibonacci = fibRek(n);
+    	
+//    	int fibonacci = fib(n);  / Iterative method
+    	
+   System.out.println("fibonacci is " + fibonacci);
+
+         }
+	
+//	Iterative method
+	
+public static int fib (int n) {
+		int a = 0;
+	 	int b = 1;
+	       
+	        for (int i = 1; i <= n; i++)
+	        {
+	          
+	            int sum = a + b;
+	            a = b;
+	            b = sum;	            
+	        }
+	        
+		return a;
+			
+	} 
+	
+	
+//	recursive method
+
+
+	public static int fibRek (int n) {
+		if (n == 0 )
+			return 0;
+		else if (n>1)
+			return fibRek(n-1)+(n-2);
+		else
+		return n;
+		
+		
+	}
+
+    }
+	
